cee5ed2699d03e6ae491e7d7013853a2bf119de5
LOKI <ompandey2341@gmail.com>
2026-07-26 17:08:33 +0530

    Force committed solution - Time: O(1), Space: O(1) static initialization, read the instructions carefully

---
+++ 13800-java-static-initializer-block-Hackerrank/13800-java-static-initializer-block.java
@@ -0,0 +1,29 @@
+public static int H;
+public static boolean flag=false;
+static{
+    Scanner ss= new Scanner(System.in);
+    B= ss.nextInt();
+    H= ss.nextInt();
+    if(B<=0||H<=0){
+        System.out.println("java.lang.Exception: Breadth and height must be 
+        positive");
+    }
+    else{
+        flag =true;
+    }
+}
+public static void main(String[] args){
+        if(flag){
+            int area=B*H;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+public static int B;
+import java.util.*;
+import java.text.*;
+import java.io.*;
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
