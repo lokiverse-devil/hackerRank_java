757869d4f09af5562e4602b7012c6398f1a23c08
LOKI <ompandey2341@gmail.com>
2026-07-26 17:17:15 +0530

    Force committed solution - Time: O(N), Space: O(N) use an extra scan.nextLine() after double input cause it creates a queue in int buffer which expects a int token and does'nt read the string ad further it is not displayed

---
+++ 9458-java-stdin-and-stdout-ii-Hackerrank/9458-java-stdin-and-stdout-ii.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        String s=scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+        double d=scan.nextDouble();
+        scan.nextLine();
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
