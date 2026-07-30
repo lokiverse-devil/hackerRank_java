aa0e57683b3142ae25abd32415eee31a1650686a
LOKI <ompandey2341@gmail.com>
2026-07-30 15:37:52 +0530

    Force committed solution - Time: O(1), Space: O(1) //Integer.toString()

---
+++ 14556-java-int-to-string-Hackerrank/14556-java-int-to-string.java
@@ -0,0 +1,29 @@
+//Write your code here
+
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+  try {
+   Scanner in = new Scanner(System.in);
+  }
+ }
+   String s=Integer.toString(n);
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+import java.util.*;
+import java.security.*;
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
